7da8ac6b147471beb39365168dc0415f37513128
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-08-18 16:14:37 -0700

    css flexbox

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -3,11 +3,27 @@ import { testData } from './data';
 import './App.css';
 
 function App() {
+  const barLegend = testData.map((ele)=>{
+    return (
+      <div className='bar-legend'>{ele.name}</div>
+    )
+  })
+  const barData = testData.map((ele)=>{
+    console.log(ele.quantity)
+    return (
+        <div className="bar-data" style={{height: ele.quantity}}/>
+    )
+  })
   return (
     <div className='App'>
       <h1>My Bar</h1>
       <div className='main-container'>
-        <div className='bar-container'></div>
+        <div className='bar-container'>
+        {barData}
+        </div>
+      </div>
+      <div className='legend-container'>
+      {barLegend}
       </div>
     </div>
   );
