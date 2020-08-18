43038768074155cda907fcd63782c85226565bf4
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-08-18 15:42:36 -0700

    bar to center div

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,24 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
+import { testData } from './data';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <h1>My Bar</h1>
+      <div className='main-container'>
+        <div className='bar-container'></div>
+      </div>
     </div>
   );
 }
---
+++ my-app/src/data.js
@@ -0,0 +1,19 @@
+export const testData = [
+    {name: 'Product 1',
+    quantity: 100},
+
+    {name:'Product 2',
+    quantity: 210},
+
+    {name:'Product 3',
+    quantity: 435},
+
+    {name:'Product 4',
+    quantity: 420},
+
+    {name:'Product 5',
+    quantity: 450},
+
+    {name:'Product 6',
+    quantity: 220}
+]
