2f3085e4baacc6391c71abf6a9ea95b0a47d1743
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-08-22 16:30:43 -0700

    styled components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,37 +1,40 @@
 import React from 'react';
 import { testData } from './data';
-import {Main, Header, OuterContainer, BarContainer, Bar, LegendContainer, Products, Text} from './reuseableStyleComp';
+import {
+  Main,
+  Header,
+  OuterContainer,
+  BarContainer,
+  Bar,
+  LegendContainer,
+  Products,
+  Text,
+  ToolTip
+} from './reuseableStyleComp';
 
 function App() {
+  const Bars = testData.map((ele, index) => {
+    return <Bar dataHeight={ele.quantity} key={index}>
+      <ToolTip> {`qty ${ele.quantity}`}</ToolTip>
+    </Bar>;
+  });
 
-  const Bars = testData.map((ele, index)=>{
-    return (
-      <Bar dataHeight={ele.quantity} key={index}></Bar>
-    )
-  })
-
-  const Product = testData.map((ele)=>{
+  const Product = testData.map(ele => {
     return (
       <Products key={ele.name}>
-        <Text>
-          {ele.name}
-        </Text>
+        <Text>{ele.name}</Text>
       </Products>
-    )
-  })
+    );
+  });
 
   return (
     <Main>
       <Header>My Bar</Header>
       <OuterContainer>
-        <BarContainer>
-          {Bars}
-        </BarContainer>
+        <BarContainer>{Bars}</BarContainer>
       </OuterContainer>
       <OuterContainer>
-        <LegendContainer>
-          {Product}
-        </LegendContainer>
+        <LegendContainer>{Product}</LegendContainer>
       </OuterContainer>
     </Main>
   );
--- my-app/src/reuseableStyleComp.js
+++ my-app/src/reuseableStyleComp.js
@@ -1,7 +1,6 @@
 import styled from 'styled-components';
 
-export const Main = styled.div`
-`;
+export const Main = styled.div``;
 
 export const Header = styled.h1`
   display: flex;
@@ -11,7 +10,7 @@ export const Header = styled.h1`
 export const OuterContainer = styled.div`
   display: flex;
   justify-content: center;
-`
+`;
 export const BarContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
@@ -21,25 +20,46 @@ export const BarContainer = styled.div`
   height: 263px;
 `;
 
+
+export const ToolTip = styled.span`
+  visibility: hidden;
+  position: absolute;
+  background-color: rgb(240, 248, 249);
+  margin-left: 50px;
+  width: 91px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 4px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
 export const Bar = styled.div`
-  background-color: rgb(208,22,65);
+  &:hover ${ToolTip} {
+    visibility: visible;
+    transition-delay: .5s;
+  }
+  background-color: rgb(208, 22, 65);
   width: 46px;
   height: ${props => `${props.dataHeight}px`};
-`
+`;
 export const LegendContainer = styled.div`
   display: flex;
   justify-content: space-evenly;
   width: 470px;
   height: 90px;
-`
+`;
+
 
 export const Products = styled.div`
   display: flex;
   width: 46px;
-`
+`;
+
 export const Text = styled.div`
- display:flex;
- align-items: center;
- transform: rotate(-90deg);
- padding-bottom: 20px;
-`
+  display: flex;
+  align-items: center;
+  transform: rotate(-90deg);
+  padding-bottom: 20px;
+`;
