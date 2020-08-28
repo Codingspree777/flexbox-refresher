c8e4d5dfd0b08fb26f55e158c1564510e61cfacf
Mike Huynh <hades@Mikes-MBP.attlocal.net>
2020-08-28 13:16:30 -0700

    Updates

---
+++ precache-manifest.4af6936eaa1b7c6c7f79ea536375d221.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5734fd6485b45b7c594b985b301a0205",
+    "url": "/flexbox-refresher/index.html"
+  },
+  {
+    "revision": "f49decd93074001334e0",
+    "url": "/flexbox-refresher/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "86081b7e40a34dff9209",
+    "url": "/flexbox-refresher/static/js/2.5ac481d0.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/flexbox-refresher/static/js/2.5ac481d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f49decd93074001334e0",
+    "url": "/flexbox-refresher/static/js/main.e789a20a.chunk.js"
+  },
+  {
+    "revision": "e7957b791248b7531ae7",
+    "url": "/flexbox-refresher/static/js/runtime-main.bf4770f2.js"
+  }
+]);
